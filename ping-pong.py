07b065cebc3253ea9b40a50ea4d7f61d0bdbb9ae
TATNET <132730574+TATNET@users.noreply.github.com>
2023-05-06 09:36:07 +0500

    Update and rename kod.py to ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -8,17 +8,7 @@ clock = time.Clock()
 FPS = 60
 
 
-game = True
-while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
 
-
-    window.fill((100,100,100))
-   
-    display.update()
-    clock.tick(FPS)
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -26,13 +16,17 @@ class GameSprite(sprite.Sprite):
  
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
-    
+ 
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-   
+
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
 
 class Player(GameSprite):
     def update_l(self):
@@ -49,3 +43,18 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
+
+
+
+
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    window.fill((100,100,100))
+   
+    display.update()
+    clock.tick(FPS)
