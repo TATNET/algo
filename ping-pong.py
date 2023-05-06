bf0e03150ce05798623fa348d557bea6ac36c602
TATNET <132730574+TATNET@users.noreply.github.com>
2023-05-06 09:45:57 +0500

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -8,23 +8,31 @@ clock = time.Clock()
 FPS = 60
 
 
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    window.fill((100,100,100))
+   
+    display.update()
+    clock.tick(FPS)
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
         sprite.Sprite.__init__(self)
-
+ 
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
- 
+    
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-
-
+   
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
-
-
-
 
 class Player(GameSprite):
     def update_l(self):
@@ -41,31 +49,3 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
-
-racket1 = Player('racket.png', 30, 200, 50, 150, 4)
-racket2 = Player('racket.png', 520, 200, 50, 150, 4)
-ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 4)
-
-
-
-
-game = True
-finish = False
-while game:
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-
-
-    if finish == False:
-        racket1.update_l()
-        racket2.update_r()
-       
-        window.fill((100,100,100))
-        racket1.reset()
-        racket2.reset()
-        ball.reset()
-
-
-    display.update()
-    clock.tick(FPS)
