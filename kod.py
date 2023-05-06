13faa5e9df228a0a54d255bb16eb8f95e39da1e0
TATNET <132730574+TATNET@users.noreply.github.com>
2023-05-06 09:32:40 +0500

    Update kod.py

--- kod.py
+++ kod.py
@@ -1,12 +1,51 @@
-spis = []
-i = input('введите чего-нибудь (stop):')
-while i != 'stop':
-    if i in spis:
-        print('это уже есть')
-    else:
-        spis.append(i)
+from pygame import *
 
-    i = input('введите чего-нибудь (stop):')
 
-print(spis)
-input()
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+clock = time.Clock()
+FPS = 60
+
+
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+
+    window.fill((100,100,100))
+   
+    display.update()
+    clock.tick(FPS)
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+ 
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+    
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+   
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
+
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
+            self.rect.y += self.speed
